c50bebbd212852ad76ad8c07b3c153abf3464dfd
NOMAANSHAIK09 <nomaanshaiknomaanshaik@gmail.com>
2026-02-13 21:35:58 +0530

    Add comment about tail recursion in calculate function
    Added a comment explaining tail recursion.

--- tailrecursion.py
+++ tailrecursion.py
@@ -1,8 +1,12 @@
+# when a function call itsefl at the last of function , no statement shold be after that than it is tail recurision 
+
+
 def calculate(n):
     if n > 0:
         k = n ** 2
         print(k)
         calculate(n - 1)
+
 
 
 calculate(4)

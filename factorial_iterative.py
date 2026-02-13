b7f7e738961e43c2957d71c56a5a5ffe01a615a8
NOMAANSHAIK09 <nomaanshaiknomaanshaik@gmail.com>
2026-02-13 21:37:42 +0530

    Refactor factorial function with consistent formatting

--- factorial_iterative.py
+++ factorial_iterative.py
@@ -1,3 +1,4 @@
+#finding  facctoeial of number using iterative function 
 def factorial_itr(n):
     fact = 1
     for i in range(1, n + 1):
@@ -8,3 +9,4 @@ def factorial_itr(n):
 num = input('Enter Number: ')
 n = int(num)
 print('Factorial: ', factorial_itr(n))
+

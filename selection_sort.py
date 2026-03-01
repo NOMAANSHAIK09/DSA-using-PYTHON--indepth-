bfc91ae08c5f5bf0a3be62eef9f8757b118f200d
NOMAANSHAIK09 <nomaanshaiknomaanshaik@gmail.com>
2026-03-01 15:44:26 +0530

    Refactor selectionsort and add comments
    Refactored selectionsort function and added comments.

--- selection_sort.py
+++ selection_sort.py
@@ -11,4 +11,7 @@ def selectionsort(A):
     return A    
 A = [64, 25, 12, 22, 11]
 print(selectionsort(A))
+"""first set n len of array , start a loop in array till n-1 from 0 index, and set the position as i after an another loop itterating in array from i+1 means index 1 it 
+compare both the index , and swap into each other"""
+
 
